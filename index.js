bd4927c6d8054e020c07db7049f4362e4944f431
rrcheatham <rrcheatham@gmail.com>
2018-05-02 08:10:35 -0400

    Intital Submit

---
+++ index.js
@@ -0,0 +1,52 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  const settings = {
+    url: YOUTUBE_SEARCH_URL,
+    data: {
+      q: `${searchTerm}`,
+      key: 'AIzaSyDrZQIOJs7kdA1lIk1aExpN01EGDfNeBh8',
+      part: 'snippet',
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callback,
+  };
+  $.ajax(settings);
+}
+
+function renderResults(result) {
+  return `
+    <div>
+      <a href='http://www.youtube.com/watch?v=${result.id.videoId}'><img src='${result.snippet.thumbnails.medium.url}'
+      ></a>
+      <h2> ${result.snippet.title} </h2>
+      <p>See more from this <a href='http://www.youtube.com/channel/${result.snippet.channelId}'>channel</a></p>
+    </div>
+  `;
+}
+
+/* function renderNav() {
+  return `
+    <div class='resultsNav'>
+      <
+    </div>
+  `
+} */
+
+function displaySearchData(data) {
+  const results = data.items.map((item, index) => renderResults(item));
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    queryTarget.val('');
+    getDataFromApi(query, displaySearchData);
+  });
+}
+
+$(watchSubmit);
